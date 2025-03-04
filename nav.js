3b94a79b59adc3464b7cbbf418abcfca119a4c34
Michael Parenti <mparenti677@gmail.com>
2025-03-03 20:24:48 -0600

    Favicon

--- nav.js
+++ nav.js
@@ -67,3 +67,13 @@ function initMobileMenuClose() {
     }
   });
 }
+
+// Scroll Changes
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector("header");
+  if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+  } else {
+      navbar.classList.remove("scrolled");
+  }
+});
