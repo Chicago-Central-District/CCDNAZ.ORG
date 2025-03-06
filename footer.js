a41bb37f7048c701a27bb6a27d8b42f194b5ec60
Michael Parenti <mparenti677@gmail.com>
2025-03-06 11:42:03 -0600

    Fix Footer Date

--- footer.js
+++ footer.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(response => response.text())
     .then(data => {
       document.getElementById("footer").innerHTML = data;
-    });
-    
-    document.getElementById("current-year").innerHTML = new Date().getFullYear();
+    }).then(() => {
+      document.getElementById("current-year").innerHTML = new Date().getFullYear();
+    })
 });
