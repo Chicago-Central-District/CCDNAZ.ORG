5b996bbc9301af498077fa16a08c03270f74b41b
Michael Parenti <mparenti677@gmail.com>
2025-03-08 12:14:38 -0600

    Mission and Vision

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const cards = document.querySelectorAll(".card");
+    const cards = document.querySelectorAll(".mission-section");
 
     function checkScroll() {
         const triggerBottom = window.innerHeight * 0.85;
         cards.forEach(card => {
             const cardTop = card.getBoundingClientRect().top;
-            if (cardTop < triggerBottom) {
+            if (window.scrollY > 100) {
+            // if (cardTop < triggerBottom) {
                 card.classList.add("in-view");
+                // console.log("done");
             }
         });
     }
