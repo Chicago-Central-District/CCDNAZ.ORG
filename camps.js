eecb746a0d2bec0a3ca07170326ac51c4a2f57b7
Michael Parenti <mparenti677@gmail.com>
2025-03-23 23:43:53 -0500

    events template draft

---
+++ camps.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeIns = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                }
+            });
+        },
+        { threshold: 0.3 }
+    );
+
+    fadeIns.forEach((el) => observer.observe(el));
+});
